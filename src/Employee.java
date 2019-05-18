e8c5b3f6620cb97c5c162be770b54fa4e8757b26
dingusagar <dingusagar@gmail.com>
2019-05-18 10:21:14 +0530

    Employee Class

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,54 @@
+import java.util.Date;
+
 public class Employee {
+    private String firstName;
+    private String lastName;
+    private Double experience;
+    private Date dateOfBirth;
+
+
+
+    public Employee() {
+    }
+
+    public Employee(String firstName, String lastName, Date dateOfBirth, Double experience) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.experience = experience;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Double getExperience() {
+        return experience;
+    }
+
+    public void setExperience(Double experience) {
+        this.experience = experience;
+    }
+
 }
